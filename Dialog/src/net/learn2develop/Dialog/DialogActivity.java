383eeccbf3f7bce330ef7ad3df19326664527ac0
wubang <wolfwubang@gmail.com>
2014-12-13 15:34:46 +0800

    add popup function

--- Dialog/src/net/learn2develop/Dialog/DialogActivity.java
+++ Dialog/src/net/learn2develop/Dialog/DialogActivity.java
@@ -1,9 +1,17 @@
 package net.learn2develop.Dialog;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class DialogActivity extends Activity {
+    CharSequence[] items={"Google","Apple","Microsoft"};
+    boolean[] itemsChecked = new boolean[items.length];
+
     /**
      * Called when the activity is first created.
      */
@@ -11,5 +19,40 @@ public class DialogActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    public void onClick(View v){
+        showDialog(0);
+    }
+    
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (0){
+            case 0:
+                return new AlertDialog.Builder(this)
+                        .setIcon(R.drawable.ic_launcher)
+                        .setTitle("This is a dialog with simple test")
+                        .setPositiveButton("OK",new DialogInterface.OnClickListener(){
+
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(getBaseContext(),"OK is clicked",Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .setNegativeButton("Cancel",new DialogInterface.OnClickListener(){
+
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(getBaseContext(),"Cancel is Clicked",Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .setMultiChoiceItems(items, itemsChecked, new DialogInterface.OnMultiChoiceClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                                Toast.makeText(getBaseContext(),items[which]+(isChecked? "checked":"unchecked"),Toast.LENGTH_SHORT).show();
+                            }
+                        }).create();
+        }
+        return null;
     }
 }

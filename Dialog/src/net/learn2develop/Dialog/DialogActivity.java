dd7aeb3470b352499dbab6434c971c60dba17fbd
wubang <wolfwubang@gmail.com>
2014-12-13 12:36:35 +0800

    init dialog activity

---
+++ Dialog/src/net/learn2develop/Dialog/DialogActivity.java
@@ -0,0 +1,15 @@
+package net.learn2develop.Dialog;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DialogActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}

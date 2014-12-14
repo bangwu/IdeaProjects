308d7a2e7904ab7e0a518fd35567fd4dcb316340
wubang <wolfwubang@gmail.com>
2014-12-14 20:41:25 +0800

    Dialog activity

--- Dialog/src/net/learn2develop/Dialog/DialogActivity.java
+++ Dialog/src/net/learn2develop/Dialog/DialogActivity.java
@@ -13,6 +13,8 @@ public class DialogActivity extends Activity {
     CharSequence[] items={"Google","Apple","Microsoft"};
     boolean[] itemsChecked = new boolean[items.length];
 
+    ProgressDialog progressDialog;
+
     /**
      * Called when the activity is first created.
      */
@@ -28,7 +30,7 @@ public class DialogActivity extends Activity {
 
     @Override
     protected Dialog onCreateDialog(int id) {
-        switch (0){
+        switch (id){
             case 0:
                 return new AlertDialog.Builder(this)
                         .setIcon(R.drawable.ic_launcher)
@@ -53,11 +55,25 @@ public class DialogActivity extends Activity {
                                 Toast.makeText(getBaseContext(),items[which]+(isChecked? "checked":"unchecked"),Toast.LENGTH_SHORT).show();
                             }
                         }).create();
+            case 1:
+                progressDialog=new ProgressDialog(this);
+                progressDialog.setIcon(R.drawable.ic_launcher);
+                progressDialog.setTitle("Downloads...");
+                progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+                progressDialog.setButton(DialogInterface.BUTTON_POSITIVE,"OK",
+                        new DialogInterface.OnClickListener(){
+
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(getBaseContext(),"Cancel clicked",Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                return progressDialog;
         }
         return null;
     }
 
-    public void btn2_onClick(View v){
+    public void btn2_onClick(View view){
         final ProgressDialog dialog=ProgressDialog.show(
                 this,"Dialog something","Please wait...",true);
         new Thread(new Runnable() {
@@ -69,6 +85,26 @@ public class DialogActivity extends Activity {
                 }catch (InterruptedException e){
                     e.printStackTrace();
                 }
+            }
+        }).start();
+    }
+
+    public void btn3_onClick(View view){
+        showDialog(1);
+        progressDialog.setProgress(0);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for(int i=1;i<=15;i++){
+                    try {
+                        Thread.sleep(5000);
+                        progressDialog.incrementProgressBy((100)/15);
+                    }catch (InterruptedException e){
+                        e.printStackTrace();
+                    }
+
+                }
+                progressDialog.dismiss();
             }
         }).start();
     }

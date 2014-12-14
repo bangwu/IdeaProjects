2a13cfae2cb9ad1b1aa64731c42a577b344b4cce
wubang <wolfwubang@gmail.com>
2014-12-14 21:27:51 +0800

    init UsingIntent

---
+++ UsingIntent/src/net/learn2develop/UsingIntent/UsingIntentActivity.java
@@ -0,0 +1,15 @@
+package net.learn2develop.UsingIntent;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class UsingIntentActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}

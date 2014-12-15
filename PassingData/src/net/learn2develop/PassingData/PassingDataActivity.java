3fb8ab8523f2fa6c66a2aa48ca5bb33496299f27
wubang <wolfwubang@gmail.com>
2014-12-15 19:25:12 +0800

    init Passing Data Activity

---
+++ PassingData/src/net/learn2develop/PassingData/PassingDataActivity.java
@@ -0,0 +1,15 @@
+package net.learn2develop.PassingData;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PassingDataActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}

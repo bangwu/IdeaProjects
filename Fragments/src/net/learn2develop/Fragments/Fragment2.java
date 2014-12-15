1f9c6218ca427d5cb743292765157a2d50cdb13f
wubang <wolfwubang@gmail.com>
2014-12-15 21:53:48 +0800

    fragment finish

---
+++ Fragments/src/net/learn2develop/Fragments/Fragment2.java
@@ -0,0 +1,18 @@
+package net.learn2develop.Fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by twer on 12/15/14.
+ */
+public class Fragment2 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment2,container,false);
+    }
+}

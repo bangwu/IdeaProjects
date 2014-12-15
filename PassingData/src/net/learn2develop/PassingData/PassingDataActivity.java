9fdde387128a165487f2ceeb2cfa3fe928aacf30
wubang <wolfwubang@gmail.com>
2014-12-15 21:03:15 +0800

    finish this function

--- PassingData/src/net/learn2develop/PassingData/PassingDataActivity.java
+++ PassingData/src/net/learn2develop/PassingData/PassingDataActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class PassingDataActivity extends Activity {
     /**
@@ -15,8 +16,29 @@ public class PassingDataActivity extends Activity {
         setContentView(R.layout.main);
     }
 
-    public void btn1_onClick(View view) {
-        Intent intent = new Intent(this, SecondActivity.class);
-        startActivity(intent);
+    public void onClick(View view) {
+        Intent intent = new Intent("net.learn2develop.PassingData.SecondActivity");
+
+        intent.putExtra("str1", "This is a string");
+        intent.putExtra("age1", "25");
+
+        Bundle bundle = new Bundle();
+
+        bundle.putString("str2", "This is a another string");
+        bundle.putInt("age2", 35);
+
+        intent.putExtras(bundle);
+        startActivityForResult(intent,1);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, data.getStringExtra("age3"), Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, data.getData().toString(), Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
--- PassingData/src/net/learn2develop/PassingData/SecondActivity.java
+++ PassingData/src/net/learn2develop/PassingData/SecondActivity.java
@@ -1,7 +1,13 @@
 package net.learn2develop.PassingData;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.net.URL;
 
 /**
  * Created by twer on 12/15/14.
@@ -11,5 +17,21 @@ public class SecondActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.secondactivity);
+
+        Toast.makeText(this,getIntent().getStringExtra("str1"),Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,getIntent().getStringExtra("age1"),Toast.LENGTH_SHORT).show();
+
+        Bundle bundle=getIntent().getExtras();
+
+        Toast.makeText(this,bundle.getString("str2"),Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,String.valueOf(bundle.getInt("age2")),Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClick(View view){
+        Intent intent=new Intent();
+        intent.putExtra("age3","45");
+        intent.setData(Uri.parse("Something passed back to main activity"));
+        setResult(RESULT_OK,intent);
+        finish();
     }
 }

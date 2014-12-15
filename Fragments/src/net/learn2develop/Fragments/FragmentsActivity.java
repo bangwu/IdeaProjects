b7d0711abe1d359c935edd0c7a112b13b3df0412
wubang <wolfwubang@gmail.com>
2014-12-15 21:21:19 +0800

    init Fragments

---
+++ Fragments/src/net/learn2develop/Fragments/FragmentsActivity.java
@@ -0,0 +1,15 @@
+package net.learn2develop.Fragments;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FragmentsActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}

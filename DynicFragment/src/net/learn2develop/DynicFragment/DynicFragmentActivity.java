3d854f451f17f7179b9e0ae29be7821c8558fedf
wubang <wolfwubang@gmail.com>
2014-12-23 23:22:01 +0800

    commit with different fragment

--- DynicFragment/src/net/learn2develop/DynicFragment/DynicFragmentActivity.java
+++ DynicFragment/src/net/learn2develop/DynicFragment/DynicFragmentActivity.java
@@ -1,7 +1,11 @@
 package net.learn2develop.DynicFragment;
 
 import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.WindowManager;
 
 public class DynicFragmentActivity extends Activity {
     /**
@@ -10,6 +14,20 @@ public class DynicFragmentActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        //setContentView(R.layout.main);
+        FragmentManager fragmentManager = this.getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        WindowManager wm = getWindowManager();
+        Display display = wm.getDefaultDisplay();
+        if (display.getWidth() > display.getHeight()) {
+            Fragment1 fragment1 = new Fragment1();
+            fragment1.getId();
+            fragmentTransaction.replace(R.id.content, fragment1);
+        } else {
+            Fragment2 fragment2 = new Fragment2();
+            fragmentTransaction.replace(R.id.content, fragment2);
+        }
+        fragmentTransaction.commit();
     }
 }
--- Fragments/src/net/learn2develop/Fragments/Fragment2.java
+++ Fragments/src/net/learn2develop/Fragments/Fragment2.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by twer on 12/15/14.
@@ -14,5 +17,19 @@ public class Fragment2 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         return inflater.inflate(R.layout.fragment2,container,false);
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        Button button = (Button) getActivity().findViewById(R.id.btnGetText);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                TextView textView = (TextView) getActivity().findViewById(R.id.lb1Fragment1);
+                Toast.makeText(getActivity(),textView.getText(),Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }

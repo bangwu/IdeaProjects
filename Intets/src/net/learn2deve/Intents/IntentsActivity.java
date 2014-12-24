bd80b09292910270245a9a1bd1a821768b1ede3a
wubang <wolfwubang@gmail.com>
2014-12-24 21:20:38 +0800

    finish Intents

--- Intets/src/net/learn2deve/Intents/IntentsActivity.java
+++ Intets/src/net/learn2deve/Intents/IntentsActivity.java
@@ -1,7 +1,10 @@
 package net.learn2deve.Intents;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class IntentsActivity extends Activity {
     /**
@@ -11,5 +14,20 @@ public class IntentsActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    public void onClickWebBrowser(View view){
+        Intent intent= new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.baidu.com"));
+        startActivity(intent);
+    }
+
+    public void onClickMakeCall(View view){
+        Intent intent= new Intent(Intent.ACTION_DIAL,Uri.parse("tel:+8618062547292"));
+        startActivity(intent);
+    }
+
+    public void onClickShowMap(View view){
+        Intent intent=new Intent(Intent.ACTION_VIEW,Uri.parse("geo:37.8888,-122.4816"));
+        startActivity(intent);
     }
 }

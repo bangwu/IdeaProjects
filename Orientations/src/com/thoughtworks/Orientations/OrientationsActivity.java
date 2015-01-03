0b8a2c53f0e227ad26419e56fa784203229ad89a
wubang <wolfwubang@gmail.com>
2015-01-03 21:17:17 +0800

    finish this function

--- Orientations/src/com/thoughtworks/Orientations/OrientationsActivity.java
+++ Orientations/src/com/thoughtworks/Orientations/OrientationsActivity.java
@@ -2,6 +2,7 @@ package com.thoughtworks.Orientations;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class OrientationsActivity extends Activity {
     /**
@@ -11,5 +12,42 @@ public class OrientationsActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        Log.d("StateInfo","onCreate()");
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d("StateInfo","onStart()");
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d("StateInfo","onResume()");
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d("StateInfo","onPause()");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d("StateInfo","onStop()");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d("StateInfo","onDestroy()");
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onRestart() {
+        Log.d("StateInfo","onRestart()");
+        super.onRestart();
     }
 }
